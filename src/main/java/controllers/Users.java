58ab5135d36d269b9d275065d04195a70478d007
92352 <56876427+92352@users.noreply.github.com>
2020-12-01 13:35:51 +0000

    Add files via upload

---
+++ src/main/java/controllers/Users.java
@@ -0,0 +1,112 @@
+package controllers;
+
+
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import server.Main;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+@Path("users/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class Users {
+    @GET
+    @Path("list")
+    public String UsersList()               //Lists all fields of all records in Users table
+    {
+        System.out.println("Invoked Users.UsersList()");
+        JSONArray response = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, SessionCookie FROM Users");
+            ResultSet results = ps.executeQuery();
+            while (results.next() == true) {
+                JSONObject row = new JSONObject();
+                row.put("UserID", results.getInt(1));
+                row.put("SessionCookie", results.getString(2));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
+
+
+    @GET
+    @Path("get/{SessionCookie}")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String GetUser(@PathParam("SessionCookie") String SessionCookie)                 //Gets the UserID of user for given SessionCookie
+    {
+        System.out.println("Invoked Users.GetUser() with SessionCookie " + SessionCookie);
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID FROM Users WHERE SessionCookie = ?");
+            ps.setString(1, SessionCookie);
+            ResultSet results = ps.executeQuery();
+
+
+            JSONObject response = new JSONObject();
+            while (results.next() == true) {
+                response.put("UserID", results.getString(1));
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
+
+    @POST
+    @Path("delete/{UserID}")
+    public String DeleteUser(@PathParam("UserID") Integer UserID) throws Exception  //deletes record from users table for given UserID
+    {
+        System.out.println("Invoked Users.DeleteUser()");
+        if (UserID == null) {
+            throw new Exception("UserID is missing in the HTTP request's URL.");
+        }
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Users WHERE UserID = ?");
+            ps.setInt(1, UserID);
+            ps.execute();
+            return "{\"OK\": \"User deleted\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to delete item, please see server console for more info.\"}";
+        }
+    }
+
+
+
+    @POST
+    @Path("add")
+    public String addUser(@FormDataParam("SessionCookie") String SessionCookie) //Creates new record in Functions tablr
+    {
+        System.out.println("Invoked Users.AddUser()");
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (SessionCookie) VALUES (?)");
+            ps.setString(1, SessionCookie);
+            ps.execute();
+            return "{\"OK\": \"Added user.\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ target/classes/client/js/client.js
@@ -0,0 +1,205 @@
+
+
+
+function plotline(x1, y1, x2, y2)       //draws a single straight line on a the canvas between two points
+{
+    var canvas = document.getElementById('canvas');         // on a canvas, the coordinate origin is in top left, so the code accounts for this
+    if (canvas.getContext) {
+        var grid = canvas.getContext('2d');
+        grid.beginPath();
+        grid.moveTo(x1, 500 - y1);
+        grid.lineTo(x2, 500 - y2);
+        grid.stroke();
+    }
+}
+
+
+
+
+
+function ClearCanvas()      //clears canvas
+{
+    var canvas = document.getElementById("canvas")
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+
+
+
+
+
+function getUser(SessionCookie) {
+    console.log("Invoked getUsers()");
+    const url = "/users/get/";    		// API method on web server will be in Users class, method list
+    fetch(url + SessionCookie, {
+        method: "GET",				//Get method
+    }).then(response => {
+        return response.json();                 //return response as JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) { //checks if response from the web server has an "Error"
+            alert(JSON.stringify(response));    // if it does, convert JSON object to string and alert (pop up window)
+        } else {
+            document.getElementById("DisplayUserID").innerHTML = response.UserID;
+        }
+    });
+}
+
+
+function getFunctions(PlottedBy) {
+    console.log("Invoked getFunctions()");
+    const url = "/functions/get/";    		// API method on web server will be in Users class, method list
+    fetch(url + PlottedBy, {
+        method: "GET",				//Get method
+    }).then(response => {
+        return response.json();                 //return response as JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) { //checks if response from the web server has an "Error"
+            alert(JSON.stringify(response));    // if it does, convert JSON object to string and alert (pop up window)
+        } else {
+            var kk = "";
+            var FunctionList = [];
+            for(var i = 0; i < response.length; i++) {  //loops through reach record returned in response
+                FunctionList[i] = response[i].FunctionID + response[i].EquationType + response[i].Parameters;
+                kk += "<li>" + FunctionList[i] + "</li>";
+            }
+            var MyList = document.getElementById('Functions');
+            MyList.innerHTML = kk;
+        }
+    });
+}
+
+
+
+
+
+function addUser(SessionCookie)
+{
+
+}
+
+
+function addFunction(FunctionID, PlottedBy, EquationType, Parameters)
+{
+
+}
+
+
+
+
+function PlotCoordinates(coordinates)
+{
+    for(i = 0; i < coordinates.length - 2; i++)
+    {
+        var x1 = coordinates[[i],[0]]
+        var y1 = coordinates [[i],[1]]
+        var x2 = coordinates[[i+1][0]]
+        var y2 = coordinates[[i+1][1]]
+        plotline(x1, y1, x2, y2)
+    }
+}
+
+
+
+
+
+
+
+
+function testplot()
+{
+    var coordinates = CalcCoordinates(100, polynomial, 1, 490, 0, 0, 0, 1, 1, 1)
+    PlotCoordinates(coordinates)
+}
+
+
+
+
+
+
+
+
+
+
+
+//Coordinate calculation functions
+
+
+function CalcCoordinates(count, type, Xleft, Xright, a, b, c, d, e, f)
+{
+    var increment = (Xright - Xleft) / count;
+    switch (type)
+    {
+        case 'polynomial':
+            return polynomial(count, increment, a, b, c, d, e, f);
+        break;
+        case 'sine':
+            return sine(count, increment, a, b, c, d, e, f);
+        break;
+        case 'cosine':
+            return cosine(count, increment, a, b, c, d, e, f);
+        break;
+        case 'tangent':
+            return tangent(count, increment, a, b, c, d, e, f);
+    }
+}
+
+
+
+
+
+function polynomial(count, increment, a, b, c, d, e, f)
+{
+    var Coordinates = [[],[]];
+    for(i = 0; i <= count; i++)
+    {
+        var CurrentX = i * increment;
+        Coordinates[[i], [0]] = CurrentX;
+        Coordinates[[i], [1]] = a*(CurrentX^5) + b*(CurrentX^4)  + c*(CurrentX^3)  + d*(CurrentX^2)  + e*(CurrentX)  + f;
+    }
+    return Coordinates;
+}
+
+
+function Sine(count, increment, a, b, c)
+{
+    var Coordinates = [[],[]];
+    for(i = 0; i <= count; i++)
+    {
+        var CurrentX = i * increment;
+        Coordinates[[i], [0]] = CurrentX;
+        Coordinates[[i], [1]] = Math.sin(CurrentX);
+    }
+    return Coordinates;
+}
+
+
+
+
+function Cosine(count, increment, a, b, c)
+{
+    var Coordinates = [[],[]];
+    for(i = 0; i <= count; i++)
+    {
+        var CurrentX = i * increment;
+        Coordinates[[i], [0]] = CurrentX;
+        Coordinates[[i], [1]] = Math.cos(CurrentX);
+    }
+    return Coordinates;
+}
+
+
+
+
+function Tangent(count, increment, a, b, c)
+{
+    var Coordinates = [[],[]];
+    for(i = 0; i <= count; i++)
+    {
+        var CurrentX = i * increment;
+        Coordinates[[i], [0]] = CurrentX;
+        Coordinates[[i], [1]] = Math.tan(CurrentX);
+    }
+    return Coordinates;
+}
